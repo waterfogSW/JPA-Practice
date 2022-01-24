a273cefde26effe8a3dc55ddb6d0907daa3ebde5
San Kim <waterfog9580@gmail.com>
2022-01-24 14:49:23 +0900

    update

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, UpdateItemDto itemDto) {
+        Book findItem = (Book) itemRepository.findOne(itemId);
+        findItem.setName(itemDto.getName());
+        findItem.setPrice(itemDto.getPrice());
+        findItem.setStockQuantity(itemDto.getStockQuantity());
+        findItem.setAuthor(itemDto.getAuthor());
+        findItem.setIsbn(itemDto.getIsbn());
     }
 
     public List<Item> findItems() {
---
+++ src/main/java/jpabook/jpashop/service/UpdateItemDto.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop.service;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateItemDto {
+    String name;
+    int price;
+    int stockQuantity;
+
+    String author;
+    String isbn;
+}
